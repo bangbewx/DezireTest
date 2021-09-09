530a7425ad876c63420f998c251e646d43bb76aa
bangbewx <70500288+bangbewx@users.noreply.github.com>
2021-09-10 00:57:21 +0700

    Add files via upload
    Test

---
+++ DezireDhimasTestApi/DezireDhimasTestApi/Model/MQueThree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DezireDhimasTestApi.Model
+{
+
+    public class MQueThree
+    {
+        public int id { get; set; }
+        public int category { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string footer { get; set; }
+        public string createdAt { get; set; }
+    }
+
+    public class ThreeResp
+    {
+        public int id { get; set; }
+        public int category { get; set; }
+        public List<Item> items { get; set; }
+        public string[] tags { get; set; }
+        public string createdAt { get; set; }
+    }
+
+    public class InputThree
+    {
+        public int id { get; set; }
+        public int category { get; set; }
+        public List<Item> items { get; set; }
+        public string createdAt { get; set; }
+    }
+
+    public class Item
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string footer { get; set; }
+    }
+
+}
