fe79fe754104518bee97682fdb13d4d36ee69c2f
bangbewx <70500288+bangbewx@users.noreply.github.com>
2021-12-02 08:35:28 +0700

    Add files via upload

---
+++ DezireDhimasTestApi/Services/IQueService.cs
@@ -0,0 +1,12 @@
+﻿using DezireDhimasTestApi.Model;
+using System.Collections.Generic;
+
+namespace DezireDhimasTestApi.Services
+{
+    public interface IQueService
+    {
+        public MQueOne GetOne();
+        public List<MQueTwo> GetTwo();
+        public List<MQueThree> PostThree();
+    }
+}
---
+++ DezireDhimasTestApi/Services/QueService.cs
@@ -0,0 +1,35 @@
+﻿using DezireDhimasTestApi.Model;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DezireDhimasTestApi.Services
+{
+    public class QueService : IQueService
+    {
+        private MQueOne _oneItems;
+        private List<MQueTwo> _twoItems;
+        private List<MQueThree> _threeItems;
+        public QueService()
+        {
+            _oneItems = new MQueOne();
+            _twoItems = new List<MQueTwo>();
+            _threeItems = new List<MQueThree>();
+        }
+
+        public MQueOne GetOne()
+        {
+            return _oneItems;
+        }
+
+        public List<MQueTwo> GetTwo()
+        {
+            return _twoItems;
+        }
+
+        public List<MQueThree> PostThree()
+        {
+            return _threeItems;
+        }
+    }
+}
